a56637cd0a1d84bc26f84957019dbb54379cf368
hobo1618 <hobdenw@gmail.com>
2024-09-10 15:10:53 +0200

    initial commit

---
+++ fish.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+# https://stackoverflow.com/questions/58597852/can-fish-shells-autosuggestion-keybindings-be-customized
+{
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      c = "clear";
+      e = "exit";
+      vim = "nvim";
+      ls = "eza";
+    };
+    functions.fish_user_key_bindings = ''
+      fish_default_key_bindings
+      bind \cn backward-search-history
+      bind \cp forward-search-history
+    '';
+  };
+}
+
---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Main Config";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixvim.url = "github:elythh/nixvim";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations = {
+        nixos = lib.nixosSystem {
+          inherit system;
+          modules = [ ./configuration.nix ];
+        };
+      };
+      homeConfigurations = {
+        willh = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+	  extraSpecialArgs = {inherit inputs;};
+          modules = [ ./home.nix ];
+        };
+      };
+    };
+}
+
+
