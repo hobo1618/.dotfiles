813f41f5f8ccfa4eebc04b563ac2ceb1852a5cd7
hobo1618 <hobdenw@gmail.com>
2024-09-11 20:23:57 +0200

    switch nixvim config to my gh

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nixvim.url = "github:elythh/nixvim";
+    nixvim.url = "github:hobo1618/nixvim";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
