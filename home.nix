7b4b5e46b5bc801d7e83dbfe8636177ade1fe32a
hobo1618 <hobdenw@gmail.com>
2024-09-18 10:35:17 +0200

    add zoom

--- home.nix
+++ home.nix
@@ -33,6 +33,7 @@
     quickemu
     tldr
     tmux
+    zoom-us
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
