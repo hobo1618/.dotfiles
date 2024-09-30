3836bbf029898b4439f578f309c1a98fcd3da126
hobo1618 <hobdenw@gmail.com>
2024-09-30 12:23:28 +0200

    add yarn

--- home.nix
+++ home.nix
@@ -34,6 +34,7 @@
     quickemu
     tldr
     tmux
+    yarn
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
