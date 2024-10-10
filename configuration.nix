6bf10607eff82bcb2cf806fade5ccf17c95d5b9b
hobo1618 <hobdenw@gmail.com>
2024-10-10 21:22:43 +0200

    back to curses plus agent config

--- configuration.nix
+++ configuration.nix
@@ -72,12 +72,12 @@ in
     hyprland.enable = true; # enable Hyprland
   };
 
-  #  services.pcscd.enable = true;
-  #  programs.gnupg.agent = {
-  #    enable = true;
-  #    enableSSHSupport = true;
-  #    pinentryPackage = pkgs.pinentry-curses;
-  #  };
+  services.pcscd.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    pinentryPackage = pkgs.pinentry-curses;
+  };
 
   environment.shells = with pkgs; [ fish ];
   users.defaultUserShell = pkgs.fish;
