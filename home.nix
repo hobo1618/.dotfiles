aeea3d52cec2c4b63b87836cc6f82610876c766d
hobo1618 <hobdenw@gmail.com>
2025-03-20 15:02:59 +0100

    add current system

--- home.nix
+++ home.nix
@@ -48,7 +48,9 @@
         url = "https://github.com/NixOS/nixpkgs/archive/nixos-24.05.tar.gz";
         sha256 = "0zydsqiaz8qi4zd63zsb2gij2p614cgkcaisnk11wjy3nmiq0x1s";
       })
-      { };
+      {
+        system = builtins.currentSystem;
+      };
   in
   [
     anki
