e3f9ecde12be8ea974ac1f9a9c34379acae48999
hobo1618 <hobdenw@gmail.com>
2025-03-20 14:58:56 +0100

    add hash

--- home.nix
+++ home.nix
@@ -41,7 +41,12 @@
 
 
   home.packages = with pkgs; let
-    oldPkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-24.05.tar.gz") { };
+    oldPkgs = import
+      (fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/nixos-24.05.tar.gz";
+        sha256 = "qiaz8qi4zd63zsb2gij2p614cgkcaisnk11wjy3nmiq0x1s";
+      })
+      { };
   in
   [
     anki
