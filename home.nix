3d23cfb6f27d174735a60b3478cc61d12072adcf
hobo1618 <hobdenw@gmail.com>
2025-03-20 15:06:40 +0100

    rm failed fixes

--- home.nix
+++ home.nix
@@ -42,17 +42,7 @@
 
 
 
-  home.packages = with pkgs; let
-    oldPkgs = import
-      (fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/nixos-24.05.tar.gz";
-        sha256 = "0zydsqiaz8qi4zd63zsb2gij2p614cgkcaisnk11wjy3nmiq0x1s";
-      })
-      {
-        system = builtins.currentSystem;
-      };
-  in
-  [
+  home.packages = with pkgs; [
     anki
     cacert
     chatgpt-cli
@@ -88,7 +78,7 @@
         obs-source-switcher
       ];
     })
-    oldPkgs.openshot-qt
+    # oldPkgs.openshot-qt
     # openshot-qt
     # libsForQt5.libopenshot
     # libsForQt5.libopenshot-audio
