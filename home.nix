ec85abd35397d18efe636f63792dabae089aace7
hobo1618 <hobdenw@gmail.com>
2025-03-20 15:01:30 +0100

    fix hash

--- home.nix
+++ home.nix
@@ -40,11 +40,13 @@
   # environment.
 
 
+
+
   home.packages = with pkgs; let
     oldPkgs = import
       (fetchTarball {
         url = "https://github.com/NixOS/nixpkgs/archive/nixos-24.05.tar.gz";
-        sha256 = "qiaz8qi4zd63zsb2gij2p614cgkcaisnk11wjy3nmiq0x1s";
+        sha256 = "0zydsqiaz8qi4zd63zsb2gij2p614cgkcaisnk11wjy3nmiq0x1s";
       })
       { };
   in
