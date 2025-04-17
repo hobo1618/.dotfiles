a9a4490481436335f510d00a21940f1241abf0b2
hobo1618 <hobdenw@gmail.com>
2025-04-17 21:46:28 +0200

    add hardware module

---
+++ modules/hardware.nix
@@ -0,0 +1,24 @@
+{ config, ... }:
+
+{
+  hardware.opengl.enable = true;
+  hardware.opengl.driSupport32Bit = true;
+
+  hardware.nvidia =
+    {
+      modesetting.enable = true;
+      powerManagement.enable = true;
+      powerManagement.finegrained = true;
+      nvidiaSettings = true;
+      open = false;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+      prime = {
+        offload.enable = true;
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+
+  hardware.nvidia-container-toolkit.enable = true;
+}
