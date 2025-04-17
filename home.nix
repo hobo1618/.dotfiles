3ec5cd4804cde8adcd6e18c27d734d077a822095
hobo1618 <hobdenw@gmail.com>
2025-04-17 15:47:04 +0200

    create script-utils

--- home.nix
+++ home.nix
@@ -1,10 +1,11 @@
-{ config, pkgs, inputs, system, ... }:
+{ pkgs, inputs, system, ... }:
 
 # Starship?
 
 let
   # Add the nixpkgs from the 24.05 channel as a flake input
   oldPkgs = import inputs.nixos-24-05 { inherit system; };
+  scriptUtils = import ./script-utils.nix { inherit pkgs; };
 in
 {
 
@@ -29,7 +30,6 @@ in
 
   services.gpg-agent = {
     enable = true;
-    #   pinentryPackage = pkgs.pinentry-curses;
     pinentryPackage = pkgs.pinentry-all;
   };
 
@@ -37,14 +37,7 @@ in
     EDITOR = "nvim";
   };
 
-  # pkgs.config.allowUnfree = true;
-
   nixpkgs.config.allowUnfree = true;
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-
-
 
   home.packages = with pkgs; [
     anki
@@ -84,9 +77,6 @@ in
       ];
     })
     oldPkgs.openshot-qt
-    # openshot-qt
-    # libsForQt5.libopenshot
-    # libsForQt5.libopenshot-audio
     pass
     pinentry-all
     poppler_utils
@@ -107,34 +97,39 @@ in
     webcamoid
     yarn
     # Define your Fish script as an executable
-    (writeScriptBin "splitmedia" ''
-      #!/usr/bin/env fish
-      # Add any runtime dependencies if necessary
-      
-      # Include the Fish script text directly
-      ${builtins.readFile ./scripts/splitmedia.fish}
-    '')
-    (writeScriptBin "pqs" ''
-      #!/usr/bin/env fish
-      # Add any runtime dependencies if necessary
-      
-      # Include the Fish script text directly
-      ${builtins.readFile ./scripts/process_question.fish}
-    '')
-    (writeScriptBin "tomov" ''
-      #!/usr/bin/env fish
-      # Add any runtime dependencies if necessary
-      
-      # Include the Fish script text directly
-      ${builtins.readFile ./scripts/convert-to-mov.fish}
-    '')
-    (writeScriptBin "movtomp4" ''
-      #!/usr/bin/env fish
-      # Add any runtime dependencies if necessary
-      
-      # Include the Fish script text directly
-      ${builtins.readFile ./scripts/mov-to-mp4.fish}
-    '')
+    (scriptUtils.mkFishScript "splitmedia" ./scripts/splitmedia.fish)
+    (scriptUtils.mkFishScript "pqs" ./scripts/process_question.fish)
+    (scriptUtils.mkFishScript "tomov" ./scripts/convert-to-mov.fish)
+    (scriptUtils.mkFishScript "movtomp4" ./scripts/mov-to-mp4.fish)
+    (scriptUtils.mkPythonScript "socr" ./scripts/python/scripts/sat-ocr.py)
+    # (writeScriptBin "splitmedia" ''
+    #   #!/usr/bin/env fish
+    #   # Add any runtime dependencies if necessary
+    #   
+    #   # Include the Fish script text directly
+    #   ${builtins.readFile ./scripts/splitmedia.fish}
+    # '')
+    # (writeScriptBin "pqs" ''
+    #   #!/usr/bin/env fish
+    #   # Add any runtime dependencies if necessary
+    #   
+    #   # Include the Fish script text directly
+    #   ${builtins.readFile ./scripts/process_question.fish}
+    # '')
+    # (writeScriptBin "tomov" ''
+    #   #!/usr/bin/env fish
+    #   # Add any runtime dependencies if necessary
+    #   
+    #   # Include the Fish script text directly
+    #   ${builtins.readFile ./scripts/convert-to-mov.fish}
+    # '')
+    # (writeScriptBin "movtomp4" ''
+    #   #!/usr/bin/env fish
+    #   # Add any runtime dependencies if necessary
+    #   
+    #   # Include the Fish script text directly
+    #   ${builtins.readFile ./scripts/mov-to-mp4.fish}
+    # '')
 
     (writers.writePython3Bin "socr"
       {
@@ -145,20 +140,6 @@ in
       } ''
       ${builtins.readFile ./scripts/python/scripts/sat-ocr.py}''
     )
-
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
   programs.ripgrep.enable = true;
@@ -173,53 +154,12 @@ in
   };
 
   programs.bun.enable = true;
-
   programs.pandoc.enable = true;
-
-  # programs.obs-studio.enable = true;
-
-  #  programs.tmux = {
-  #    enable = true;
-  #    extraConfig = ''
-  #      set-option -g prefix C-a
-  #      unbind-key C-b
-  #    '';
-  #  };
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+  home.file = { };
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/willh/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
---
+++ script-utils.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+
+let
+  mkFishScript = name: path:
+    pkgs.writeScriptBin name ''
+      #!/usr/bin/env fish
+      ${builtins.readFile path}
+    '';
+
+  mkPythonScript = name: path:
+    pkgs.writers.writePython3Bin name
+      {
+        libraries = [
+          pkgs.python312Packages.openai
+          pkgs.python312Packages.pydantic
+        ];
+      } ''
+      ${builtins.readFile path}
+    '';
+in
+{
+  inherit mkFishScript mkPythonScript;
+}
