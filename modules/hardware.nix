79412a90c65e8e6bb9cf34f3d78abf5d7c8d4fe8
hobo1618 <hobdenw@gmail.com>
2025-04-19 20:50:42 +0200

    add gpu reset service

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -19,5 +19,17 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
+  systemd.services.nvidia-reset = {
+    description = "Force NVIDIA GPU reset before resume";
+    before = [ "suspend.target" ];
+    wantedBy = [ "suspend.target" ];
+    serviceConfig.Type = "oneshot";
+    script = ''
+      echo "Resetting NVIDIA before suspend"
+      echo 1 > /sys/bus/pci/devices/0000:01:00.0/remove
+      sleep 1
+      echo 1 > /sys/bus/pci/rescan
+    '';
+  };
 
 }
